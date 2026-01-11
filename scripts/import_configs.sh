aa128645688dca1f8b7c1460b942ee6153361d8f
r3g1s <21206554+reg1z@users.noreply.github.com>
2026-01-10 23:41:34 -0800

    Fix conflicting binds

--- scripts/import_configs.sh
+++ scripts/import_configs.sh
@@ -5,10 +5,16 @@ HYPR_CONFIG="$USER_HOME/.config/hypr"
 
 cd ..
 
+cp -f input.conf $HYPR_CONFIG/input.conf
+
+# cp -f bindings.conf $HYPR_CONFIG/bindings.conf
 cp -f bindings-overrides.conf $HYPR_CONFIG/bindings-overrides.conf
 cp -f bindings-submap-vm-passthru.conf $HYPR_CONFIG/bindings-submap-vm-passthru.conf
-cp -f input.conf $HYPR_CONFIG/input.conf
-cp -f master-layout.conf $HYPR_CONFIG/master-layout.conf
-cp -f monitors.conf $HYPR_CONFIG/monitors.conf
 cp -f bindings-speechnote.conf $HYPR_CONFIG/bindings-speechnote.conf
+
+cp -f master-layout.conf $HYPR_CONFIG/master-layout.conf
+
+cp -f monitors.conf $HYPR_CONFIG/monitors.conf
+
+cp -f windows.conf $HYPR_CONFIG/windows.conf
 cp -f windowrules-overrides.conf $HYPR_CONFIG/windowrules-overrides.conf
