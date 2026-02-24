714dd90679df92adb5a9f3e284792b173127977b
r3g1s <21206554+reg1z@users.noreply.github.com>
2026-02-24 15:52:35 -0800

    Added custom backgrounds aligned with default omarchy themes.

--- scripts/import-backgrounds.sh
+++ scripts/import-backgrounds.sh
@@ -1,3 +1,26 @@
 #!/bin/sh
+# Import custom backgrounds into
+# omarchy's default themes.
 
-cp -f ../backgrounds/0-nord.png ~/.local/share/omarchy/themes/nord/backgrounds/0-nord.png
+themes=(
+  "catppuccin"
+  "catppuccin-latte"
+  "ethereal"
+  "everforest"
+  "flexoki-light"
+  "gruvbox"
+  "hackerman"
+  "kanagawa"
+  "matte-black"
+  "nord"
+  "osaka-jade"
+  "ristretto"
+  "rose-pine"
+  "tokyo-night"
+)
+
+for theme in "${themes[@]}"; do
+  for i in 00 01 02 03 04 05 06 07 08 09; do
+    cp -f ../backgrounds/$i-$theme.png ~/.local/share/omarchy/themes/$theme/backgrounds/$i-$theme.png
+  done
+done
