9442a6e205d360baa7d262aabdcec87bb5ab8388
reg1z <21206554+reg1z@users.noreply.github.com>
2026-02-26 19:08:47 -0800

    Added firefox setup script. Sets firefox as the default browser, disables telemetry, installs ublock origin, and instantiates profiles. Sets firefox as the browser for PWAs

---
+++ scripts/setup-firefox.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# Sets up firefox as the default browser.
+# Firefox will be used PWAs in place of chromium.
+# System-wide policy will automatically install
+# ublock-origin and disable telemetry across all
+# profiles.
+
+USER_HOME=$(eval echo "~${SUDO_USER:-$USER}")
+FFOX_CFG="$USER_HOME/.mozilla/firefox/"
+FFOX_DOTS="../firefox"
+
+# Import policies .json (enforces ublock origin, disabled telemetry, etc. system-wide)
+sudo mkdir -p /etc/firefox/policies
+sudo cp -f $FFOX_DOTS/policies.json /etc/firefox/policies/policies.json
+sudo chown root:root /etc/firefox/policies/policies.json
+sudo chmod 644 /etc/firefox/policies/policies.json
+
+# Install firefox if not present
+yay -Sy --needed firefox
+
+#########################
+### Browser Profiles: ###
+
+# PWAs
+PROFILE="WebApps"
+firefox CreateProfile "$PROFILE $FFOX_CFG/$PROFILE"
+cp -RTf "$FFOX_DOTS/templates/$PROFILE/" "$FFOX_CFG/$PROFILE/"
+
+# Personal
+PROFILE="Personal"
+firefox -CreateProfile "$PROFILE $FFOX_CFG/$PROFILE"
+cp -RTf "$FFOX_DOTS/templates/$PROFILE/" "$FFOX_CFG/$PROFILE/"
+
+# Work
+PROFILE="Work"
+firefox -CreateProfile "$PROFILE $FFOX_CFG/$PROFILE"
+cp -RTf "$FFOX_DOTS/templates/$PROFILE/" "$FFOX_CFG/$PROFILE/"
+
+###                   ###
+#########################
+
+
+# Replace omarchy-launch-webapp with ours (backing up the old)
+mv ~/.local/share/omarchy/bin/omarchy-launch-webapp ~/.local/share/omarchy/bin/omarchy-launch-webapp.bak
+cp -f $FFOX_DOTS/omarchy-launch-webapp.sh ~/.local/share/omarchy/bin/omarchy-launch-webapp
+sudo chmod 755 ~/.local/share/omarchy/bin/omarchy-launch-webapp
+
+# Set firefox as default browser
+xdg-settings set default-web-browser firefox.desktop
+
+echo "fin"
