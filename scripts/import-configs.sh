e3a0fbc17ae020f2ca9cd10e5622fda1a442be48
reg1z <21206554+reg1z@users.noreply.github.com>
2026-02-28 23:51:12 -0800

    Now we are working with symlinks. Still keeping the import script as an emergency backup.

--- scripts/import-configs.sh
+++ scripts/import-configs.sh
@@ -8,33 +8,40 @@ cd ..
 ########################
 ####### Hyprland #######
 ###                  ###
+
+# Dotfiles
+USER_DOTS="$USER_HOME/repos/omarchy-overrides"
+HYPR_DOTS="$USER_DOTS/hypr"
+
+# Local
 HYPR_CFG="$USER_HOME/.config/hypr"
 HYPR_SCRIPTS="$HYPR_CFG/scripts"
 
-cp -f input.conf $HYPR_CFG/input.conf
-cp -f bindings.conf $HYPR_CFG/bindings.conf
-cp -f bindings-submap-vm-passthru.conf $HYPR_CFG/bindings-submap-vm-passthru.conf
+cp -f $HYPR_DOTS/input.conf $HYPR_CFG/input.conf
+cp -f $HYPR_DOTS/bindings.conf $HYPR_CFG/bindings.conf
+cp -f $HYPR_DOTS/bindings-submap-vm-passthru.conf $HYPR_CFG/bindings-submap-vm-passthru.conf
 
 ### Deprecated
 # cp -f bindings-overrides.conf $HYPR_CFG/bindings-overrides.conf
 # cp -f bindings-speechnote.conf $HYPR_CFG/bindings-speechnote.conf
 
-cp -f layout-master.conf $HYPR_CFG/layout-master.conf
+cp -f $HYPR_DOTS/layout-master.conf $HYPR_CFG/layout-master.conf
+cp -f $HYPR_DOTS/layout-dwindle.conf $HYPR_CFG/layout-dwindle.conf
 
-cp -f monitors.conf $HYPR_CFG/monitors.conf
+cp -f $HYPR_DOTS/monitors.conf $HYPR_CFG/monitors.conf
 
-cp -f windows.conf $HYPR_CFG/windows.conf
-cp -f autostart.conf $HYPR_CFG/autostart.conf
-cp -f envs.conf $HYPR_CFG/envs.conf
-cp -f looknfeel.conf $HYPR_CFG/looknfeel.conf
+cp -f $HYPR_DOTS/windows.conf $HYPR_CFG/windows.conf
+cp -f $HYPR_DOTS/autostart.conf $HYPR_CFG/autostart.conf
+cp -f $HYPR_DOTS/envs.conf $HYPR_CFG/envs.conf
+cp -f $HYPR_DOTS/looknfeel.conf $HYPR_CFG/looknfeel.conf
 
 # scripts
 mkdir -p $HYPR_SCRIPTS
-cp -f scripts/hyprscripts/hyprgamemode.sh $HYPR_SCRIPTS/hyprgamemode.sh
-cp -f scripts/hyprscripts/delta-resize.sh $HYPR_SCRIPTS/delta-resize.sh
-cp -f scripts/hyprscripts/orientation-cycle.sh $HYPR_SCRIPTS/orientation-cycle.sh
-cp -f scripts/hyprscripts/master-roll.sh $HYPR_SCRIPTS/master-roll.sh
-cp -f scripts/hyprscripts/center-mfact-daemon.sh $HYPR_SCRIPTS/center-mfact-daemon.sh
+cp -f $USER_DOTS/scripts/hyprscripts/hyprgamemode.sh $HYPR_SCRIPTS/hyprgamemode.sh
+cp -f $USER_DOTS/scripts/hyprscripts/delta-resize.sh $HYPR_SCRIPTS/delta-resize.sh
+cp -f $USER_DOTS/scripts/hyprscripts/orientation-cycle.sh $HYPR_SCRIPTS/orientation-cycle.sh
+cp -f $USER_DOTS/scripts/hyprscripts/master-roll.sh $HYPR_SCRIPTS/master-roll.sh
+cp -f $USER_DOTS/scripts/hyprscripts/center-mfact-daemon.sh $HYPR_SCRIPTS/center-mfact-daemon.sh
 chmod 755 $HYPR_SCRIPTS/*.sh
 
 ###                  ###
@@ -48,15 +55,15 @@ chmod 755 $HYPR_SCRIPTS/*.sh
 # tmux
 TMUX_CFG="$USER_HOME/.config/tmux"
 mkdir -p $TMUX_CFG
-cp -f tmux/tmux.conf $TMUX_CFG/tmux.conf
+cp -f $USER_DOTS/tmux/tmux.conf $TMUX_CFG/tmux.conf
 
 # fcitx5
 FCITX5_CFG="$USER_HOME/.config/fcitx5/conf"
 mkdir -p $FCITX5_CFG
-cp -f fcitx5/spell.conf $FCITX5_CFG/spell.conf
+cp -f $USER_DOTS/fcitx5/spell.conf $FCITX5_CFG/spell.conf
 
 # Waybar
-cp -f waybar/config.jsonc $USER_HOME/.config/waybar/config.jsonc
+cp -f $USER_DOTS/waybar/config.jsonc $USER_HOME/.config/waybar/config.jsonc
 
 ###                  ###
 ######## Other #########
--- scripts/link-configs.sh
+++ scripts/link-configs.sh
@@ -40,35 +40,47 @@ mkdir -p $WAYBAR_CFG
 
 backup() {
   # Check for existing backups. Exit if present.
-  for f in $HYPR_LOCAL/*.conf; do
-    if [[ "$f" == "*.bak" ]]; then
-      echo -e "Backups already present.\Ending execution."
-      exit 0
+  for f in $HYPR_LOCAL/*; do
+    name=$(basename "$f")
+    echo $name
+    if [[ "$name" == *.bak ]]; then
+      echo "Backup already present. Ending execution."
+      exit 1
     fi
   done
 
   # Make backups for each file that corresponds
   # to a custom config.
   for f in $HYPR_DOTS/*.conf; do
-    cp "$HYPR_LOCAL/${f}" "$HYPR_LOCAL/${f}.bak"
+    name=$(basename "$f") # extract lone file name w/ "basename"
+    cp -v "$HYPR_LOCAL/$name" "$HYPR_LOCAL/$name.bak"
   done
+
+  cp -v $TMUX_CFG/tmux.conf $TMUX_CFG/tmux.conf.bak # tmux
+  cp -v $FCITX5_CFG/spell.conf $FCITX5_CFG/spell.conf.bak # fcitx5
+  cp -v $WAYBAR_CFG/config.jsonc $WAYBAR_CFG/config.jsonc.bak # waybar
 }
 
-restore {
+restore() {
+  echo -e "Restoring backups...\n"
   for f in $HYPR_DOTS/*.conf; do
-    mv "$HYPR_LOCAL/${f}.bak" "$HYPR_LOCAL/${f}"
+    name=$(basename "$f") # extract lone file name w/ "basename"
+    mv -v "$HYPR_LOCAL/$name.bak" "$HYPR_LOCAL/$name"
   done
+
+  mv -v $TMUX_CFG/tmux.conf.bak $TMUX_CFG/tmux.conf # tmux
+  mv -v $FCITX5_CFG/spell.conf.bak $FCITX5_CFG/spell.conf # fcitx5
+  mv -v $WAYBAR_CFG/config.jsonc.bak $WAYBAR_CFG/config.jsonc # waybar
+  echo -e "\nBackups restored!"
 }
 
 link() {
-  # sever, then relink
-  sever;
-
   # Hyprland Configs
 	ln -s -v $HYPR_DOTS/input.conf $HYPR_LOCAL/input.conf
 	ln -s -v $HYPR_DOTS/bindings.conf $HYPR_LOCAL/bindings.conf
 	ln -s -v $HYPR_DOTS/bindings-submap-vm-passthru.conf $HYPR_LOCAL/bindings-submap-vm-passthru.conf
 	ln -s -v $HYPR_DOTS/layout-master.conf $HYPR_LOCAL/layout-master.conf
+	ln -s -v $HYPR_DOTS/layout-dwindle.conf $HYPR_LOCAL/layout-dwindle.conf
 	ln -s -v $HYPR_DOTS/monitors.conf $HYPR_LOCAL/monitors.conf
 	ln -s -v $HYPR_DOTS/windows.conf $HYPR_LOCAL/windows.conf
 	ln -s -v $HYPR_DOTS/autostart.conf $HYPR_LOCAL/autostart.conf
@@ -93,22 +105,28 @@ link() {
 }
 
 sever() {
+  echo -e "\nSevering links..."
   # Hyprland Configs
   rm -v $HYPR_LOCAL/input.conf
+  rm -v $HYPR_LOCAL/bindings.conf
   rm -v $HYPR_LOCAL/bindings-submap-vm-passthru.conf
   rm -v $HYPR_LOCAL/layout-master.conf
+  rm -v $HYPR_LOCAL/layout-dwindle.conf
   rm -v $HYPR_LOCAL/monitors.conf
   rm -v $HYPR_LOCAL/windows.conf
   rm -v $HYPR_LOCAL/autostart.conf
   rm -v $HYPR_LOCAL/envs.conf
   rm -v $HYPR_LOCAL/looknfeel.conf
 
+  # We don't need to worry about unlinking these really.
+  # Maybe one day.
+  #
   # Hyprland Scripts
-  rm -v $HYPR_LOCAL_SCRIPTS/hyprgamemode.sh
-  rm -v $HYPR_LOCAL_SCRIPTS/delta-resize.sh
-  rm -v $HYPR_LOCAL_SCRIPTS/orientation-cycle.sh
-  rm -v $HYPR_LOCAL_SCRIPTS/master-roll.sh
-  rm -v $HYPR_LOCAL_SCRIPTS/center-mfact-daemon.sh
+  # rm -v $HYPR_LOCAL_SCRIPTS/hyprgamemode.sh
+  # rm -v $HYPR_LOCAL_SCRIPTS/delta-resize.sh
+  # rm -v $HYPR_LOCAL_SCRIPTS/orientation-cycle.sh
+  # rm -v $HYPR_LOCAL_SCRIPTS/master-roll.sh
+  # rm -v $HYPR_LOCAL_SCRIPTS/center-mfact-daemon.sh
 
   # tmux
   rm -v $TMUX_CFG/tmux.conf
@@ -117,7 +135,10 @@ sever() {
   rm -v $FCITX5_CFG/spell.conf
 
   # Waybar
-  rm -v $USER_HOME/.config/waybar.jsonc
+  rm -v $WAYBAR_CFG/config.jsonc
+
+  echo -e "\nLinks severed!"
+  sleep 5
 }
 
 reload_env() {
@@ -141,18 +162,17 @@ elif [[ "$1" == "restore" ]]; then
   echo -e "Restoring files in $HYPR_LOCAL..."
   sever;
   restore;
-  exit 0
 elif [[ "$1" == "sever" ]]; then
-  echo -e "Severing links.\nThe environment will not be reloaded.\n"
+  echo -e "Severing links.\nThe environment will not be reloaded."
   sever;
   restore;
-  exit 0
 elif [[ "$1" == "link" ]]; then
-  echo -e "Linking configs.\nThe environment will be reloaded.\n"
+  backup;
   sever;
+  echo -e "Linking configs.\nThe environment will be reloaded."
   link;
 else
-  echo "No input specified.\nPlease enter 'sever', 'link', 'backup', or 'restore'.\n"
+  echo -e "\nNo input specified.\nPlease enter 'sever', 'link', 'backup', or 'restore'."
   exit 0
 fi
 
