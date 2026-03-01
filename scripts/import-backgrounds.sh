4f23a25d1d0fd1252ab88b045b5e568156fd4668
reg1z <21206554+reg1z@users.noreply.github.com>
2026-02-28 22:10:33 -0800

    Added ability to remove backgrounds from system config

--- scripts/import-backgrounds.sh
+++ scripts/import-backgrounds.sh
@@ -2,6 +2,10 @@
 # Import custom backgrounds into
 # omarchy's default themes.
 
+USER_HOME=$(eval echo "~${SUDO_USER:-$USER}")
+USER_DOTS=$USER_HOME/repos/omarchy-overrides
+OMARCHY=$USER_HOME/.local/share/omarchy
+
 themes=(
   "catppuccin"
   "catppuccin-latte"
@@ -22,8 +26,29 @@ themes=(
   "miasma"
 )
 
-for theme in "${themes[@]}"; do
-  for i in 00 01 02 03 04 05 06 07 08 09; do
-    cp -f ../backgrounds/$i-$theme.png ~/.local/share/omarchy/themes/$theme/backgrounds/$i-$theme.png
+BACKGROUNDS=$USER_DOTS/backgrounds
+
+# If adding "sever" as the first argument,
+# unlink backgrounds.
+if [[ "$1" == "sever"  ]]; then
+
+  for theme in "${themes[@]}"; do
+    for i in 00 01 02 03 04 05 06 07 08 09; do
+      if [[ -f $BACKGROUNDS/$i-$theme.png ]]; then
+        rm -v "$OMARCHY/themes/$theme/backgrounds/$i-$theme.png"
+      fi
+    done
   done
-done
+
+# Else, link them.
+else
+
+  for theme in "${themes[@]}"; do
+    for i in 00 01 02 03 04 05 06 07 08 09; do
+      if [[ -f $BACKGROUNDS/$i-$theme.png ]]; then
+        cp -v "$USER_DOTS/backgrounds/$i-$theme.png" "$OMARCHY/themes/$theme/backgrounds/$i-$theme.png"
+      fi
+    done
+  done
+
+fi
