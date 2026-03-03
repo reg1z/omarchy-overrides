942a96152fbca264db58b722d7e2fda07516e5ca
reg1z <21206554+reg1z@users.noreply.github.com>
2026-03-03 11:28:46 -0800

    Update

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,6 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.scrolloff = 999
+vim.opt.scrolloff = 0
 vim.opt.autoread = true
 vim.opt.relativenumber = true
